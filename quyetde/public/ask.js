8d36eb29a13440b07807a3af2841ea3341dd3be4
naruto12308 <kun_duc123@yahoo.com.vn>
2019-07-31 21:14:24 +0700

    Tuan 4

--- quyetde/public/ask.js
+++ quyetde/public/ask.js
@@ -1,11 +1,45 @@
-function Word_counter()
-{
-    const wordElement = document.getElementById('question');
-    const word = wordElement.value;
-    let x = 200 - word.length;
-    const textareaElement = document.getElementById('w-counter');
-    textareaElement.innerText = `Con lai ${x}/200`
-}
+window.onload = () => {
+    
+    const submitButton = document.querySelector('.submit-button');
+    console.log(submitButton);
+    if(submitButton)
+    {
+        submitButton.addEventListener('click',(event)  => {
+        const textAreaValue = document.querySelector('.question').value;
+        event.preventDefault();
+        fetch('/create-question',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                questionContent: textAreaValue,
+            }),
+        })
+            .then((response)=>{
+                // response.json() only when server reponse with json
+                // response.text() only when server response with string
+                return response.json();
+            })
+            .then((data)=>{
+                // xu ly response data
+                console.log('Data: ',data);
+            })
+            .catch((error)=>{
+                console.log(error);
+                window.alert(error.message);
+            });
+        });
+    }
+    function Word_counter()
+    {
+        const wordElement = document.querySelector('.question');
+        const word = wordElement.value;
+        let x = 200 - word.length;
+        const textareaElement = document.getElementById('w-counter');
+        textareaElement.innerText = `Con lai ${x}/200`
+    }
 
-const question = document.querySelector('#question');
-question.addEventListener('input',Word_counter);
+    const question = document.querySelector('.question');
+    question.addEventListener('input',Word_counter);
+}
--- quyetde/server.js
+++ quyetde/server.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
-    res.send("hahaha");
+    res.sendFile(path.resolve(__dirname,'./public/ask.html'));
 });
 
 app.get('/ask', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'./public/ask.html'));
+});
+
+app.post('/create-question',(req,res)=>{
+    const newQuestion = {
+        content:  req.body.questionContent,
+        like: 0,
+        dislike: 0,
+        id: new Date().getTime(),
+    };
+    
+    fs.readFile('data.json',{encoding: 'utf8'},(error,data)=>{
+        if(error){
+            res.status(500).json({
+                success: false,
+                message: error.message,
+            });
+        }
+        else {
+            const questions = JSON.parse(data);
+            questions.push(newQuestion);
+
+            fs.writeFile('data.json', JSON.stringify(questions), (err) => {
+                if(err){
+                    res.status(500).json({
+                        success: false,
+                        message: error.message,
+                    });
+                }
+                else {
+                    res.status(201).json({
+                        success: true,
+                        data: newQuestion,
+                    });
+                }
+            });
+        }
+    });
+
+
 });
 
 app.listen(3000, (error)=>{
@@ -20,5 +62,4 @@ app.listen(3000, (error)=>{
     else{
         console.log('Dang nghe o cong 3000...');
     }
-
 });
