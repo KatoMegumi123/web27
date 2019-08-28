3272d195f340d33df2755bc82e79bc46d5a91467
naruto12308 <kun_duc123@yahoo.com.vn>
2019-08-28 19:46:06 +0700

    react minihack

---
+++ minihack-react/src/pages/CreateGameScreen.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateGameScreen extends Component {
+    render() {
+        return (
+            <div>
+                Creat Game
+            </div>
+        );
+    }
+}
+
+export default CreateGameScreen;
---
+++ minihack-react/src/pages/GameDetailScreeen.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameDetailScreeen extends Component {
+    render() {
+        return (
+            <div>
+                Game Detail
+            </div>
+        );
+    }
+}
+
+export default GameDetailScreeen;
